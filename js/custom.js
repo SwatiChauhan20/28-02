01da4d68bc55cc5311bec2a2754a3629552463b7
swati chauhan <chauhan.swati104@gmail.com>
2019-11-11 17:12:06 +0530

    first commit

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+// mdc.textField.MDCTextField.attachTo(document.querySelector('.t1'));
+// mdc.textField.MDCTextField.attachTo(document.querySelector('.t2'));
+
+// mdc.textField.MDCTextField.attachTo(document.querySelector('.text1'));
+// mdc.textField.MDCTextField.attachTo(document.querySelector('.text2'));
+// mdc.textField.MDCTextField.attachTo(document.querySelector('.text3'));
+// mdc.textField.MDCTextField.attachTo(document.querySelector('.text4'));
+// mdc.textField.MDCTextField.attachTo(document.querySelector('.text5'));
+mdc.select.MDCSelect.attachTo(document.querySelector('.select_menu'));
